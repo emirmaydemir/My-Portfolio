e626df5f66834849c496881827f2830141f879a2
Emir Muhammet Aydemir <emirmaydemir@gmail.com>
2025-04-06 23:15:10 +0300

    herosection update

--- portfolio-website/components/UI/HeroSection.jsx
+++ portfolio-website/components/UI/HeroSection.jsx
@@ -10,10 +10,10 @@ const HeroSection = () => {
     <section className="lg:py-16">
       <div className="grid grid-cols-1 sm:grid-cols-12">
         <motion.div initial={{ opacity: 0, scale: 0.5 }} animate={{ opacity: 1, scale: 1 }} transition={{ duration: 0.5 }} className="col-span-8 place-self-center text-center sm:text-left justify-self-start">
-          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-8xl lg:leading-normal font-extrabold">
+          <h1 className="text-white mb-4 text-4xl sm:text-5xl lg:text-7xl lg:leading-normal font-extrabold">
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-primary-400 to-secondary-600">Hello, I&apos;m </span>
             <br></br>
-            <TypeAnimation sequence={["Judy", 1000, "Web Developer", 1000, "Mobile Developer", 1000, "UI/UX Designer", 1000]} wrapper="span" speed={50} repeat={Infinity} />
+            <TypeAnimation sequence={["Full Stack Developer", 1000, "Back-End Developer", 1000, "Front-End Developer", 1000, "UI/UX Designer", 1000, "Web Developer", 1000, "Mobile Developer", 1000, "ERP Software Developer", 1000, "Emir Muhammet Aydemir", 1000]} wrapper="span" speed={50} repeat={Infinity} />
           </h1>
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptuous.</p>
           <div>
