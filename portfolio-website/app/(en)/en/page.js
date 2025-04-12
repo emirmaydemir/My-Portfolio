caf9220a8f31fa44fce42117e94a06344bafffa5
Emir Muhammet Aydemir <emirmaydemir@gmail.com>
2025-04-13 00:07:45 +0300

    update

--- portfolio-website/app/(en)/en/page.js
+++ portfolio-website/app/(en)/en/page.js
@@ -1,26 +1,28 @@
 import HeroSection from "@/components/UI/HeroSection";
-import Navbar from "@/components/UI/Navbar";
 import AboutSection from "@/components/UI/AboutSection";
 import ProjectsSection from "@/components/UI/ProjectsSection";
 import EmailSection from "@/components/UI/EmailSection";
 import Footer from "@/components/UI/Footer";
+import ExperienceSection from "@/components/UI/ExperienceSection";
 import AchievementsSection from "@/components/UI/AchievementsSection";
 import HeroData from "@/locales/en/heroData.json";
 import AboutData from "@/locales/en/aboutData.json";
+import ProjectData from "@/locales/en/projectData.json";
 
 export default function Home() {
   const heroData = HeroData.heroData;
   const aboutData = AboutData.aboutData;
   const certifications = AboutData.certifications;
   const publications = AboutData.publications;
+  const projectData = ProjectData.projectData;
   return (
-    <main className="flex min-h-screen flex-col bg-[#121212]">
-      <Navbar />
+    <main className="flex min-h-screen flex-col bg-white text-black dark:bg-[#121212] dark:text-white transition-colors duration-300">
       <div className="container mt-24 mx-auto px-12 py-4">
         <HeroSection heroContent={heroData} />
         <AchievementsSection />
         <AboutSection aboutContent={aboutData} certifications={certifications} publications={publications} />
-        <ProjectsSection />
+        <ProjectsSection projectContent={projectData} />
+        <ExperienceSection />
         <EmailSection />
       </div>
       <Footer />
