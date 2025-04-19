7d2d79d3a9fb5a265326d632a8fd055cf3fedd3f
Emir Muhammet Aydemir <emirmaydemir@gmail.com>
2025-04-19 20:03:52 +0300

    sitemap update

--- portfolio-website/app/sitemap.js
+++ portfolio-website/app/sitemap.js
@@ -4,7 +4,7 @@ import path from "path";
 //export const revalidate = 60;
 
 export default function sitemap() {
-  const baseUrl = "https://emiraydemir.com/"; // Burayı canlı URL ile değiştirin
+  const baseUrl = "https://emiraydemir.com"; // Burayı canlı URL ile değiştirin
 
   // JSON dosyasının yolunu belirtin
   const filePath = path.join(process.cwd(), "locales", "tr", "projectData.json");
